cda091e71a8e8b53a467b5aee25e207a54231774
Bole05 <bole.wu@estudiantat.upc.edu>
2026-03-31 14:51:15 +0200

    Test Texture

--- src/SavePoint.cpp
+++ src/SavePoint.cpp
@@ -16,8 +16,8 @@ bool SavePoint::Awake(){
 }
 
 bool SavePoint::Start() {
-	texture = Engine::GetInstance().textures->Load("Assets/Textures/player1.png");
-	//std::unordered_map<int, std::string>aliase = { {0,"off"},{1,"on"} };
+	texture = Engine::GetInstance().textures->Load("Assets/Textures/SavePoint_Test/Off.png");
+	//std::unordered_map<int, std::string>aliase = { {0,"Off"},{1,"On"} };
 
 	// Savepoint sensor: Activates when the player passes through it. The physics engine sends an OnCollision / OnTrigger notification
 	pbody = Engine::GetInstance().physics->CreateRectangleSensor((int)position.getX(), (int)position.getY(), 32, 32, bodyType::STATIC);// Static object, 32x32 size
