c149e900fca8c9f9d75e2f17f349193aafb91aea
Bole05 <bole.wu@estudiantat.upc.edu>
2026-05-20 00:20:15 +0200

    Dip spritesheet

---
+++ Assets/Textures/Entities/Enemies/Dip/SS_Dip.tsx
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="SS_Dip" tilewidth="512" tileheight="256" tilecount="154" columns="22">
+ <image source="SS_Dip.png" width="11264" height="1792"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="44">
+  <animation>
+   <frame tileid="44" duration="100"/>
+   <frame tileid="45" duration="100"/>
+   <frame tileid="46" duration="100"/>
+   <frame tileid="47" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="66">
+  <animation>
+   <frame tileid="66" duration="100"/>
+   <frame tileid="67" duration="100"/>
+   <frame tileid="68" duration="100"/>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="70" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="88">
+  <animation>
+   <frame tileid="88" duration="100"/>
+   <frame tileid="89" duration="100"/>
+   <frame tileid="90" duration="100"/>
+   <frame tileid="91" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="110">
+  <animation>
+   <frame tileid="110" duration="100"/>
+   <frame tileid="111" duration="100"/>
+   <frame tileid="112" duration="100"/>
+   <frame tileid="113" duration="100"/>
+   <frame tileid="114" duration="100"/>
+   <frame tileid="115" duration="100"/>
+   <frame tileid="116" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="132">
+  <animation>
+   <frame tileid="132" duration="100"/>
+   <frame tileid="133" duration="100"/>
+   <frame tileid="134" duration="100"/>
+   <frame tileid="135" duration="100"/>
+   <frame tileid="136" duration="100"/>
+   <frame tileid="137" duration="100"/>
+   <frame tileid="138" duration="100"/>
+   <frame tileid="139" duration="100"/>
+   <frame tileid="140" duration="100"/>
+   <frame tileid="141" duration="100"/>
+   <frame tileid="142" duration="100"/>
+   <frame tileid="143" duration="100"/>
+   <frame tileid="144" duration="100"/>
+   <frame tileid="145" duration="100"/>
+   <frame tileid="146" duration="100"/>
+   <frame tileid="147" duration="100"/>
+   <frame tileid="148" duration="100"/>
+   <frame tileid="149" duration="100"/>
+   <frame tileid="150" duration="100"/>
+   <frame tileid="151" duration="100"/>
+   <frame tileid="152" duration="100"/>
+   <frame tileid="153" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
