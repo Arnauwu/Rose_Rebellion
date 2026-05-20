843db22bf729ce828dfc530d70cf4fd12aae8f8e
Bole05 <bole.wu@estudiantat.upc.edu>
2026-05-20 10:37:27 +0200

    SpriteSheet Particulas

---
+++ Assets/Textures/Particles/SS_particulas_polvo.tsx
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="SS_particulas_polvo" tilewidth="256" tileheight="256" tilecount="64" columns="8">
+ <image source="SS_particulas_polvo.png" width="2048" height="2048"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="80"/>
+   <frame tileid="1" duration="80"/>
+   <frame tileid="2" duration="80"/>
+   <frame tileid="3" duration="80"/>
+   <frame tileid="4" duration="80"/>
+   <frame tileid="5" duration="80"/>
+   <frame tileid="6" duration="80"/>
+   <frame tileid="7" duration="80"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="80"/>
+   <frame tileid="9" duration="80"/>
+   <frame tileid="10" duration="80"/>
+   <frame tileid="11" duration="80"/>
+   <frame tileid="12" duration="80"/>
+   <frame tileid="13" duration="80"/>
+   <frame tileid="14" duration="80"/>
+   <frame tileid="15" duration="80"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <animation>
+   <frame tileid="16" duration="80"/>
+   <frame tileid="17" duration="80"/>
+   <frame tileid="18" duration="80"/>
+   <frame tileid="19" duration="80"/>
+   <frame tileid="20" duration="80"/>
+   <frame tileid="21" duration="80"/>
+   <frame tileid="22" duration="80"/>
+   <frame tileid="23" duration="80"/>
+   <frame tileid="24" duration="80"/>
+  </animation>
+ </tile>
+</tileset>
