607f2de3076da5af1c2077b9ad4f4bd9ef906e4c
yangfeng-xu <yangfeng.xu@estudiantat.upc.edu>
2026-05-21 09:47:38 +0200

    player move fx

--- src/Player.h
+++ src/Player.h
@@ -237,7 +237,7 @@ private:
 
 	bool wasWalking = false;
 	float stepTimer = 0.0f;
-	float timeBetweenSteps = 0.35f;
+	float timeBetweenSteps = 15.5f;
 	float footX = 0.0f;
 	float footY = 0.0f;
 	float lookDownTimer = 0.0f;
