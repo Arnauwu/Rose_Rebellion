3cae65bf252847192d22c730bb719e36e1010983
JanCorredor <jan.corredor@estudiantat.upc.edu>
2026-05-21 08:52:40 +0200

    Dragon + Navigation Layers

--- src/GameManager.h
+++ src/GameManager.h
@@ -25,7 +25,7 @@ struct GameState {
     bool stDownAttack = false;
 
     // World
-    std::string currentMap = "Nexo.tmx";
+    std::string currentMap = "Mountain_03.tmx";
     Vector2D playerPosition = { 2147.0f, 912.0f }; // Spawn inicial por defecto
 
     std::vector<std::string> openedDoors;
