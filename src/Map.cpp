1d585522ea49be4f37c94c31b2fa8a26c8b0d509
Marc Jiménez <marc.jimenez.marquina@estudiantat.upc.edu>
2026-05-21 03:59:37 +0200

    Arreglos pequeños catacumbas + animación de la plataforma móvil + poner entidades catacumbas

--- src/Map.cpp
+++ src/Map.cpp
@@ -629,6 +629,8 @@ bool Map::Load(std::string path, std::string fileName)
 			{
 				for (const auto& obj : objectsGroups->objects)
 				{
+					if (obj->width <= 0 || obj->height <= 0) continue;
+
 					PhysBody* collider;
 					if (objectsGroups->properties.GetProperty("Sensor") != NULL and objectsGroups->properties.GetProperty("Sensor")->value) // Trigger
 					{
@@ -822,7 +824,7 @@ bool Map::Load(std::string path, std::string fileName)
 					colliderList.push_back(collider);
 				}
 			}
-			else if (objectsGroups->properties.GetProperty("Polygon") != NULL && objectsGroups->properties.GetProperty("Polygon")->value) // NUEVO: POLYGON
+			else if (objectsGroups->properties.GetProperty("Polygon") != NULL && objectsGroups->properties.GetProperty("Polygon")->value)
 			{
 				for (const auto& obj : objectsGroups->objects)
 				{
