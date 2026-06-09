c8a8c2676d42855440e2975f80319e35cd9b80d3
Bole05 <bole.wu@estudiantat.upc.edu>
2026-06-09 16:26:37 +0200

    Trunco spritesheet

---
+++ Assets/Textures/Animation/Trunco/Atlas_troncos_romper_montaña.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="Atlas_troncos_romper_montaña" tilewidth="128" tileheight="386" tilecount="20" columns="10">
+ <image source="Atlas_troncos_romper_montaña.png" width="1285" height="1025"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="11" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
