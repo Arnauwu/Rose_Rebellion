a2d271f925342cc89ad3978e3476b9b5654430f7
Bole05 <bole.wu@estudiantat.upc.edu>
2026-06-09 23:56:12 +0200

    Dragon proyectil

---
+++ Assets/Textures/Entities/Enemies/Dragon/proyectilDragonImprovisado.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="proyectilDragonImprovisado" tilewidth="256" tileheight="256" tilecount="4" columns="4">
+ <image source="proyectilDragonImprovisado.png" width="1024" height="256"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
