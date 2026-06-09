0bf512511fd09727c5d9389d18c12417a034ffe7
Bole05 <bole.wu@estudiantat.upc.edu>
2026-06-09 22:40:20 +0200

    New wood assets

--- Assets/Textures/Animation/Trunco/Atlas_troncos_romper_montana.tsx
+++ Assets/Textures/Animation/Trunco/Atlas_troncos_romper_montana.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="Atlas_troncos_romper_montana" tilewidth="128" tileheight="386" tilecount="20" columns="10">
- <image source="Atlas_troncos_romper_montana.png" width="1285" height="1025"/>
+<tileset version="1.10" tiledversion="1.12.0" name="Atlas_troncos_romper_montana" tilewidth="128" tileheight="384" tilecount="6" columns="3">
+ <image source="Atlas_troncos_romper_montana.png" width="384" height="768"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="100"/>
@@ -16,19 +16,19 @@
    <frame tileid="2" duration="100"/>
   </animation>
  </tile>
- <tile id="10">
+ <tile id="3">
   <animation>
-   <frame tileid="10" duration="100"/>
+   <frame tileid="3" duration="100"/>
   </animation>
  </tile>
- <tile id="11">
+ <tile id="4">
   <animation>
-   <frame tileid="11" duration="100"/>
+   <frame tileid="4" duration="100"/>
   </animation>
  </tile>
- <tile id="12">
+ <tile id="5">
   <animation>
-   <frame tileid="12" duration="100"/>
+   <frame tileid="5" duration="100"/>
   </animation>
  </tile>
 </tileset>
