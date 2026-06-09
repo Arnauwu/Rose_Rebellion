8c6141bd5a94d38ad590c6d702776c659398e36a
yangfeng-xu <yangfeng.xu@estudiantat.upc.edu>
2026-06-09 23:18:44 +0200

    update

--- Assets/Textures/Entities/Princess/SS_Princesa.tsx
+++ Assets/Textures/Entities/Princess/SS_Princesa.tsx
@@ -1,6 +1,148 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="SS_Princesa" tilewidth="256" tileheight="256" tilecount="228" columns="12">
- <image source="SS_Princesa.png" width="3072" height="5119"/>
+<tileset version="1.10" tiledversion="1.11.2" name="SS_Princesa" tilewidth="256" tileheight="256" tilecount="240" columns="12">
+ <image source="SS_Princesa.png" width="3072" height="5120"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="110"/>
+   <frame tileid="1" duration="110"/>
+   <frame tileid="2" duration="110"/>
+   <frame tileid="3" duration="110"/>
+   <frame tileid="4" duration="110"/>
+   <frame tileid="5" duration="110"/>
+   <frame tileid="6" duration="110"/>
+   <frame tileid="7" duration="110"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="110"/>
+   <frame tileid="13" duration="110"/>
+   <frame tileid="14" duration="110"/>
+   <frame tileid="15" duration="110"/>
+   <frame tileid="16" duration="110"/>
+   <frame tileid="17" duration="110"/>
+   <frame tileid="18" duration="110"/>
+   <frame tileid="19" duration="110"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="110"/>
+   <frame tileid="25" duration="110"/>
+   <frame tileid="36" duration="110"/>
+   <frame tileid="37" duration="110"/>
+   <frame tileid="38" duration="110"/>
+  </animation>
+ </tile>
+ <tile id="36">
+  <animation>
+   <frame tileid="36" duration="110"/>
+   <frame tileid="37" duration="110"/>
+   <frame tileid="38" duration="110"/>
+  </animation>
+ </tile>
+ <tile id="48">
+  <animation>
+   <frame tileid="48" duration="110"/>
+   <frame tileid="49" duration="110"/>
+   <frame tileid="60" duration="110"/>
+   <frame tileid="61" duration="110"/>
+   <frame tileid="62" duration="110"/>
+  </animation>
+ </tile>
+ <tile id="60">
+  <animation>
+   <frame tileid="60" duration="110"/>
+   <frame tileid="61" duration="110"/>
+   <frame tileid="62" duration="110"/>
+  </animation>
+ </tile>
+ <tile id="72">
+  <animation>
+   <frame tileid="72" duration="110"/>
+   <frame tileid="73" duration="110"/>
+   <frame tileid="74" duration="110"/>
+   <frame tileid="75" duration="110"/>
+   <frame tileid="76" duration="110"/>
+   <frame tileid="77" duration="110"/>
+   <frame tileid="78" duration="110"/>
+   <frame tileid="79" duration="110"/>
+   <frame tileid="80" duration="110"/>
+   <frame tileid="81" duration="110"/>
+   <frame tileid="82" duration="110"/>
+   <frame tileid="83" duration="110"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <animation>
+   <frame tileid="84" duration="110"/>
+   <frame tileid="85" duration="110"/>
+   <frame tileid="86" duration="110"/>
+   <frame tileid="87" duration="110"/>
+   <frame tileid="88" duration="110"/>
+   <frame tileid="89" duration="110"/>
+   <frame tileid="90" duration="110"/>
+   <frame tileid="91" duration="110"/>
+   <frame tileid="92" duration="110"/>
+   <frame tileid="93" duration="110"/>
+   <frame tileid="94" duration="110"/>
+   <frame tileid="95" duration="110"/>
+  </animation>
+ </tile>
+ <tile id="96">
+  <animation>
+   <frame tileid="216" duration="110"/>
+   <frame tileid="217" duration="110"/>
+   <frame tileid="218" duration="110"/>
+   <frame tileid="219" duration="110"/>
+   <frame tileid="220" duration="110"/>
+   <frame tileid="96" duration="110"/>
+   <frame tileid="97" duration="110"/>
+   <frame tileid="98" duration="110"/>
+   <frame tileid="99" duration="110"/>
+   <frame tileid="100" duration="110"/>
+   <frame tileid="101" duration="110"/>
+   <frame tileid="102" duration="110"/>
+   <frame tileid="103" duration="110"/>
+   <frame tileid="104" duration="110"/>
+   <frame tileid="105" duration="110"/>
+   <frame tileid="106" duration="110"/>
+   <frame tileid="107" duration="110"/>
+   <frame tileid="108" duration="110"/>
+   <frame tileid="109" duration="110"/>
+   <frame tileid="110" duration="110"/>
+   <frame tileid="111" duration="110"/>
+   <frame tileid="112" duration="110"/>
+   <frame tileid="113" duration="110"/>
+  </animation>
+ </tile>
+ <tile id="120">
+  <animation>
+   <frame tileid="228" duration="110"/>
+   <frame tileid="229" duration="110"/>
+   <frame tileid="230" duration="110"/>
+   <frame tileid="231" duration="110"/>
+   <frame tileid="232" duration="110"/>
+   <frame tileid="120" duration="110"/>
+   <frame tileid="121" duration="110"/>
+   <frame tileid="122" duration="110"/>
+   <frame tileid="123" duration="110"/>
+   <frame tileid="124" duration="110"/>
+   <frame tileid="125" duration="110"/>
+   <frame tileid="126" duration="110"/>
+   <frame tileid="127" duration="110"/>
+   <frame tileid="128" duration="110"/>
+   <frame tileid="129" duration="110"/>
+   <frame tileid="130" duration="110"/>
+   <frame tileid="131" duration="110"/>
+   <frame tileid="132" duration="110"/>
+   <frame tileid="133" duration="110"/>
+   <frame tileid="134" duration="110"/>
+   <frame tileid="135" duration="110"/>
+   <frame tileid="136" duration="110"/>
+   <frame tileid="137" duration="110"/>
+  </animation>
+ </tile>
  <tile id="168">
   <animation>
    <frame tileid="168" duration="110"/>
@@ -34,15 +176,6 @@
    <frame tileid="207" duration="110"/>
    <frame tileid="208" duration="110"/>
    <frame tileid="209" duration="110"/>
-  </animation>
- </tile>
- <tile id="216">
-  <animation>
-   <frame tileid="216" duration="110"/>
-   <frame tileid="217" duration="110"/>
-   <frame tileid="218" duration="110"/>
-   <frame tileid="219" duration="110"/>
-   <frame tileid="220" duration="110"/>
   </animation>
  </tile>
 </tileset>
--- Assets/Textures/UI/Buttons/Alas_Movimiento_V2.tsx
+++ Assets/Textures/UI/Buttons/Alas_Movimiento_V2.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Alas_Movimiento_V2" tilewidth="416" tileheight="71" tilecount="16" columns="4">
+<tileset version="1.10" tiledversion="1.11.2" name="Alas_Movimiento_V2" tilewidth="416" tileheight="70" tilecount="20" columns="4">
  <image source="Alas_Movimiento_V2.png" width="2079" height="354"/>
- <tile id="0">
+ <tile id="5">
   <animation>
    <frame tileid="0" duration="110"/>
    <frame tileid="1" duration="110"/>
@@ -19,6 +19,8 @@
    <frame tileid="13" duration="110"/>
    <frame tileid="14" duration="110"/>
    <frame tileid="15" duration="110"/>
+   <frame tileid="16" duration="110"/>
+   <frame tileid="17" duration="110"/>
   </animation>
  </tile>
 </tileset>
