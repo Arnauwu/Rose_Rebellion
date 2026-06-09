87eb2ec41197534c870814c4f7d3fe2595d9d746
Bole05 <bole.wu@estudiantat.upc.edu>
2026-06-09 21:53:31 +0200

    Door Anims

--- Assets/Textures/Animation/Door/SS_puertas.tsx
+++ Assets/Textures/Animation/Door/SS_puertas.tsx
@@ -3,14 +3,6 @@
  <image source="SS_puertas.png" width="2048" height="2048"/>
  <tile id="0">
   <animation>
-   <frame tileid="0" duration="100"/>
-   <frame tileid="1" duration="100"/>
-   <frame tileid="2" duration="100"/>
-   <frame tileid="3" duration="100"/>
-   <frame tileid="4" duration="100"/>
-   <frame tileid="5" duration="100"/>
-   <frame tileid="6" duration="100"/>
-   <frame tileid="7" duration="100"/>
    <frame tileid="8" duration="100"/>
    <frame tileid="9" duration="100"/>
    <frame tileid="10" duration="100"/>
