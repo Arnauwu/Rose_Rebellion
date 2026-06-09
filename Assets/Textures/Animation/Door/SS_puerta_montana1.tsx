f8af56400afd9e34ae8200bf2cf07c1a80b1f987
Bole05 <bole.wu@estudiantat.upc.edu>
2026-06-09 17:52:04 +0200

    Use updated mountain door animation

--- Assets/Textures/Animation/Door/SS_puerta_montana1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="SS_puerta_montana" tilewidth="256" tileheight="439" tilecount="16" columns="8">
- <image source="SS_puerta_montana.png" width="2048" height="1024"/>
-</tileset>
